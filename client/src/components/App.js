d792b3ee913a7098733eaf174177863ba6a1ac8e
Ismael Perez <ismaelperezemail@gmail.com>
2019-01-05 16:48:39 -0700

    -Initialized app -Created basic stream components -Set up and wired up routes with react router

---
+++ client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Route path="/" exact component={StreamList}/>
+                        <Route path="/streams/new" component={StreamCreate}/>
+                        <Route path="/streams/edit" component={StreamEdit}/>
+                        <Route path="/streams/delete" component={StreamDelete}/>
+                        <Route path="/streams/show" component={StreamShow}/>
+                    </div>
+                </BrowserRouter>
+            </div>
+        )
+    }
+}
+
+export default App
